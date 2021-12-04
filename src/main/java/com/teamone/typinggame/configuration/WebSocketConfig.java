7183d09490bc2ab9a0c6acbf17d6f1257ee5af10
Ksenia <grass.xenia@gmail.com>
2021-12-04 13:19:02 -0600

    updated allowed origins and gameplay controller method to print input integers

--- src/main/java/com/teamone/typinggame/configuration/WebSocketConfig.java
+++ src/main/java/com/teamone/typinggame/configuration/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/new-player").setAllowedOrigins("https://infallible-snyder-60164a.netlify.app", "http://localhost:3000").withSockJS();
+        registry.addEndpoint("/new-player").setAllowedOrigins("https://infallible-snyder-60164a.netlify.app", "http://localhost:3000", "https://space-racer.netlify.app").withSockJS();
     }
 }
